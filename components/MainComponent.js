59d10294681adfce6bbd50375f1c952772752549
Kerry <trankerry94@gmail.com>
2021-05-21 17:21:10 -0700

    Using Redux

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -10,6 +10,15 @@ import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
+import { connect } from 'react-redux';
+import { fetchCampsites, fetchComments, fetchPromotions, fetchPartners } from '../redux/ActionCreators';
+
+const mapDispatchToProps = {
+    fetchCampsites,
+    fetchComments,
+    fetchPartners,
+    fetchPromotions
+};
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -197,6 +206,14 @@ const MainNavigator = createDrawerNavigator(
 const AppNavigator = createAppContainer(MainNavigator)
 
 class Main extends Component {
+
+    componentDidMount() {
+        this.props.fetchCampsites();
+        this.props.fetchComments();
+        this.props.fetchPartners();
+        this.props.fetchPromotions();
+    }   
+    
     render() {
         return (
             <View style={{
@@ -237,4 +254,4 @@ const styles = StyleSheet.create({
         fontSize: 24
     }
 })
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
