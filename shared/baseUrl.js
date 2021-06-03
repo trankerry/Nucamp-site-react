e6ba3efb5b020fc2a704a4bee95e90e56e002b5e
Kerry <trankerry94@gmail.com>
2021-06-02 22:01:04 -0700

    updated wifi

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,6 +1,6 @@
 //work wifi
-export const baseUrl = 'http://10.246.194.238:3001/';
+// export const baseUrl = 'http://10.246.194.238:3001/';
 
 //home wifi
-// export const baseUrl = 'http://192.168.0.5.:3001/';
+export const baseUrl = 'http://192.168.0.5.:3001/';
 
